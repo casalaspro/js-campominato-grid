d30cc2d8df6ad5a7c452b19ebc0e05bf9f4d6832
casalaspro <casalaspro.alessandro@gmail.com>
2024-03-14 11:25:40 +0100

    ADD - reset

--- js/app.js
+++ js/app.js
@@ -30,19 +30,72 @@ const buttonElement = document.querySelector("button");
 // console.log(buttonElement);
 // creo nodo contenitore
 const containerElement = document.querySelector(".grid");
+
+// variabile di presenza celle
+let areCells = false;
+// containerElement.innerHTML = "";
 // console.log(containerElement);
 
+// const selectElement = document.getElementById("difficulty");
+// const selectValue = parseInt(selectElement.value);
+// console.dir(selectElement);
+
+// let numberCells;
+// if(selectValue === 1){
+//   numberCells = 100;
+// }else if(selectValue === 2){
+//   numberCells = 81;
+// }else if(selectValue === 3){
+//   numberCells = 49;
+// }else{
+//   console.log("Choose a difficulty before to star the game.")
+// }
+
+
+
 // attribuire eventListener al bottone play che:
-buttonElement.addEventListener("click", function(){
+buttonElement.addEventListener("click", function () {
+  const selectElement = document.getElementById("difficulty");
+  const selectValue = parseInt(selectElement.value);
+  console.log(selectValue);
+  
+  // debugger;
+
+  // reset grid
+  containerElement.innerHTML = '';
+  // if(areCells === true){
+  //   let cellsElements = document.getElementsByClassName("cell");
+  //   console.dir(cellsElements);
+  //   cellsElements[1].remove();
+  // }
+
+  // console.dir(parseInt(selectValue));
   // console.log("Play");
-  for(let i = 1; i<=100; i++){
+  let numberCells;
+  let difficultyClass;
+
+  if (selectValue === 100) {
+    numberCells = selectValue;
+    difficultyClass = "difficulty-1"
+  } else if (selectValue === 81) {
+    numberCells = selectValue;
+    difficultyClass = "difficulty-2"
+  } else if (selectValue === 49) {
+    numberCells = selectValue;
+    difficultyClass = "difficulty-3"
+  } else {
+    console.log("Choose a difficulty before to star the game.")
+  }
+  
+  for (let i = 1; i <= selectValue; i++) {
+    // containerElement.innerHTML = "";
     // console.log(i);
     let number = i;
     const divElement = document.createElement("div");
-    divElement.classList.add("cell");
+    divElement.classList.add("cell", difficultyClass);
     const textElement = document.createTextNode(number);
     divElement.appendChild(textElement);
-    divElement.addEventListener("click", function(){
+    divElement.addEventListener("click", function () {
       this.classList.toggle('cyan');
       console.log(number);
     });
@@ -51,16 +104,19 @@ buttonElement.addEventListener("click", function(){
 
 
   }
+  areCells = true;
 })
 
+areCells = true;
 
 
-  // crei un ciclo da 1 a 100 che:
-    // crei un div
-    // gli aggiunga la classe .cell
-    // crei de testo
-    // appenda il teso al div
-    // appenda il div al contenitore
-    // aggiunga un eventListener al div che:
-      // aggiunga o tolga la classe .cyan
-      // scriva in console il numero della cella cliccata
+
+// crei un ciclo da 1 a 100 che:
+// crei un div
+// gli aggiunga la classe .cell
+// crei de testo
+// appenda il teso al div
+// appenda il div al contenitore
+// aggiunga un eventListener al div che:
+// aggiunga o tolga la classe .cyan
+// scriva in console il numero della cella cliccata
